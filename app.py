dc90894132b7f0eb40b0937c91b4daa937b9a24a
Shreyansh Pathak <shreyanshp132@gmail.com>
2026-09-12 16:08:01 +0530

    feat: implement stateful feature engineering for device attempt counts

--- app.py
+++ app.py
@@ -10,11 +10,20 @@ class TransactionPayload(BaseModel):
     merchant_mcc:Annotated[int,Field(...,)]
     ip_address:Annotated[str,Field(...,)]
     timestamp:Annotated[str,Field(...,)]
+
+device_counts={}
 @app.post('/predict')
 def payload(payload:TransactionPayload):
     payment=payload.model_dump()
+    device_id=payment['device_id']
+    if device_id in device_counts:
+        device_counts[device_id]+=1
+    else:
+        device_counts[device_id]=1
+    payment['device_attempt_count']=device_counts[device_id]
     print(payment)
     return {
         'status':'successful',
          'message':'transaction received'
     }
+
